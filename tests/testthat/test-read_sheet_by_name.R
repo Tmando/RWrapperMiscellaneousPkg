e6e1be4065e4777f8616078530c2780af8fc9cf5
Thomas Mandorfer <t.mandorfer@gmail.com>
2023-07-09 14:06:30 +0200

    update tests

--- tests/testthat/test-read_sheet_by_name.R
+++ tests/testthat/test-read_sheet_by_name.R
@@ -1,5 +1,6 @@
 test_that("Read excel sheet by name", {
   sheet_output <- read_sheet_by_name("read_sheet_by_name/example_input.xlsx", "Test1")
-  out_df <- jsonlite::fromJSON(sheet_output)
+  out_df <- do.call(rbind.data.frame, sheet_output)
+  print(names(out_df))
   expect_equal(names(out_df),c("am","carb","cyl","disp","drat","gear","hp","mpg","qsec","vs","wt"))
 })
